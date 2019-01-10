004436bfbe3bf8f951d8bd5db2d17ced30d93f35
asm12 <asalamorales@gmail.com>
2019-01-10 17:05:45 +0100

    Added DigitalSignature test implementations

--- ProvesUnitaries/test/data/DigitalSignatureTest.java
+++ ProvesUnitaries/test/data/DigitalSignatureTest.java
@@ -1,7 +1,10 @@
 package data;
 
-
 import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 public class DigitalSignatureTest {
 
@@ -15,10 +18,26 @@ public class DigitalSignatureTest {
 
     }
 
-    @Test
-    public void test(){
-
+    @Test (expected = Exception.class)
+    public void testNullException() throws Exception{
+        DigitalSignature nullExample = new DigitalSignature(null);
     }
+
+    @Test
+    public void testNotNull() throws Exception {
+        DigitalSignature notNullExample = new DigitalSignature(new byte[10]);
+        assertNotNull(notNullExample);
+    }
+    @Test
+    public void testCorrectKeyPrint() throws Exception {
+
+        byte[] bytes = new byte[10];
+        DigitalSignature printExample = new DigitalSignature(bytes);
+
+        String expected = "DigitalSignature{" + "key='" + bytes.toString() + '\'' + '}';
+        assertEquals(expected, printExample.toString());
+    }
+
 
     @After
     public void teardown(){
