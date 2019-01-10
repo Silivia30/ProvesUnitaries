64d180c3ca37138504c8f9cdc7d771a2d7fef4eb
asm12 <asalamorales@gmail.com>
2019-01-10 18:22:37 +0100

    Added NifTest implementation

--- ProvesUnitaries/test/data/NifTest.java
+++ ProvesUnitaries/test/data/NifTest.java
@@ -2,6 +2,8 @@ package data;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class NifTest {
 
     @BeforeClass
@@ -14,9 +16,30 @@ public class NifTest {
 
     }
 
-    @Test
-    public void test(){
+    @Test (expected = Exception.class)
+    public void testNullException() throws Exception{
+        Nif nullExample = new Nif("");
+    }
 
+    @Test
+    public void testValidNif() throws Exception {
+        Nif validNif = new Nif("47980090C");
+        assertEquals("47980090C", validNif.getNIF());
+    }
+
+    @Test (expected = Exception.class)
+    public void testInvalidNifLower() throws Exception{
+        Nif invalidExample = new Nif("123123123C");
+    }
+
+    @Test (expected = Exception.class)
+    public void testInvalidNifUpper() throws Exception{
+        Nif invalidExample = new Nif("123123C");
+    }
+
+    @Test (expected = Exception.class)
+    public void testInvalidNifUnordered() throws Exception{
+        Nif invalidExample = new Nif("C12312312");
     }
 
     @After
