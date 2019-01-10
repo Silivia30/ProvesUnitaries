6c9ba71e2970be75738a3777dd06fed4ceb72a0b
asm12 <asalamorales@gmail.com>
2019-01-10 17:44:43 +0100

    Added MailAddressTest and a TODO (create custom exception: InvalidMailException)

--- ProvesUnitaries/test/data/MailAddressTest.java
+++ ProvesUnitaries/test/data/MailAddressTest.java
@@ -2,6 +2,9 @@ package data;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
+
 public class MailAddressTest {
 
     @BeforeClass
@@ -14,9 +17,21 @@ public class MailAddressTest {
 
     }
 
-    @Test
-    public void test(){
+    @Test (expected = Exception.class)
+    public void testNullException() throws Exception{
+        MailAddress nullExample = new MailAddress("");
+    }
 
+    @Test
+    public void testNotNull() throws Exception {
+        MailAddress notNullExample = new MailAddress("test@test.com");
+        assertEquals("test@test.com", notNullExample.getMailAdress());
+    }
+
+    @Test (expected = Exception.class)
+    public void testIncorrectMail() throws Exception {
+//    TODO: create custom exception InvalidMailException
+        MailAddress incorrectMail = new MailAddress("incorrect.@@.mail.com");
     }
 
     @After
